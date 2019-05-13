32443e81f1954a57ca7de9583d516d916ba72b52
Tyler Ray <tylerray@Tylers-MacBook-Pro.local>
2019-05-13 12:06:19 -0700

    Load CSVs

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plot
 import numpy as np
+
+games = pd.read_csv('games.csv');
+wellness = pd.read_csv('wellness.csv');
+rpe = pd.read_csv('rpe.csv');
+gps = pd.read_csv('gps.csv');
