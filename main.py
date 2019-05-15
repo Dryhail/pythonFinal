c2c6f7d00ed60e9beccaa5d9e2deb87d915510aa
tylerray <tylerray@live.com>
2019-05-15 12:15:38 -0700

    Made something print out

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import numpy as np
 games = pd.read_csv('games.csv');
 wellness = pd.read_csv('wellness.csv');
 rpe = pd.read_csv('rpe.csv');
-#gps = pd.read_csv('gps.csv');  You need to download this yourself. ver 25mb#
+gps = pd.read_csv('gps.csv'); #You need to download this yourself. ver 25mb#
 
 print("making sure this prints out")
+
+print(games.head());
+print(wellness.head());
+print(rpe.head());
+print(gps.head());
