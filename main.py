91992da14b5b2f208bfad0a8880b0050825daa48
rickymartinez <rickym372@yahoo.com>
2019-05-15 12:32:43 -0700

    remove print statement

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import numpy as np
 games = pd.read_csv('games.csv');
 wellness = pd.read_csv('wellness.csv');
 rpe = pd.read_csv('rpe.csv');
-gps = pd.read_csv('gps.csv'); #You need to download this yourself. ver 25mb#
+#gps = pd.read_csv('gps.csv'); #You need to download this yourself. ver 25mb#
 
-print("making sure this prints out")
 
 print(games.head());
 print(wellness.head());
 print(rpe.head());
-print(gps.head());
+#print(gps.head());
