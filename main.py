61a05334da675780ca11c478e0c71679d8ead224
wheeoo <jlamothe@gmail.com>
2019-05-18 13:38:56 -0700

    #total count of Nutrition values(Excellent v Okay v Poor) #AIM:  plot excellent nutrition and poor nutrition vs.

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ wellness = pd.read_csv('wellness.csv');
 rpe = pd.read_csv('rpe.csv');
 #gps = pd.read_csv('gps.csv'); #You need to download this yourself. ver 25mb#
 
-
+#total count of Nutrition values(Excellent v Okay v Poor)
+#AIM:  plot excellent nutrition and poor nutrition vs.
+print(wellness.Nutrition.value_counts(sort=True))
 wellness.loc[wellness["Nutrition"] == "Poor"]
