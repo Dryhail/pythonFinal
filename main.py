05883467bdec8d81a5b14d25d748c58cb198eb38
rickymartinez <rickym372@yahoo.com>
2019-05-18 17:04:35 -0700

    shows how player nutrition effects outcome of game

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 games = pd.read_csv('games.csv');
 wellness = pd.read_csv('wellness.csv');
 rpe = pd.read_csv('rpe.csv');
-#gps = pd.read_csv('gps.csv'); #You need to download this yourself. ver 25mb#
+gps = pd.read_csv('gps.csv'); #You need to download this yourself. ver 25mb#
 
 #total count of Nutrition values(Excellent v Okay v Poor)
 #AIM:  plot excellent nutrition and poor nutrition vs.
@@ -17,3 +17,9 @@ wellness.loc[wellness["Nutrition"] == "Poor"]
 
 #checks all players who have a desire greater or equal to 5, almost all have a good game readiness
 wellness.loc[wellness["Desire"] >= 5].sort_values(by="PlayerID")
+
+#this is one of the games where they win, more than half of the team had excellent nutrition
+wellness.loc[wellness["Date"]=="2017-11-30"]
+
+#thisis one of the games where they lost, they had half or less players with excellent nutrition
+wellness.loc[wellness["Date"]=="2017-12-01"]
